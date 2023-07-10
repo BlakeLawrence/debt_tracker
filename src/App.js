9eb461dc2c294f8f5a6e3bcda2bfc7405edd7f1b
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-07-10 08:07:53 +0100

    playing with state for inputs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import Button from "./components/Button";
@@ -10,6 +11,9 @@ import {
 import TotalsModal from "./components/TotalsModal";
 
 function App() {
+  const [debt, setDebt] = useState(0);
+  const [item, setItem] = useState("");
+
   return (
     <div className="w-full h-screen bg-blue-100">
       <div className="flex justify-center p-5">
@@ -20,6 +24,7 @@ function App() {
         type="number"
         inputStyling={incomeInput}
         placeholder="enter debt"
+        onChange={(event) => setDebt(event.target.value)}
       />
       <Button buttonStyling={buttonEnter} />
       <h2 className="ml-2">What is your salary after tax?</h2>
@@ -35,6 +40,7 @@ function App() {
           type="text"
           inputStyling={incomeInput}
           placeholder="enter item"
+          onChange={(event) => setItem(event.target.value)}
         />
         <Input
           type="number"
@@ -44,7 +50,7 @@ function App() {
         <Button buttonStyling={buttonDanger} />
       </div>
       <div>
-        <TotalsModal />
+        <TotalsModal item={item} debt={debt} />
       </div>
     </div>
   );
--- src/components/TotalsModal.jsx
+++ src/components/TotalsModal.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 
-const TotalsModal = () => {
+const TotalsModal = ({ item, debt }) => {
   return (
-    <div className="w-1/2 h-1/2 bg-[#fff] rounded-lg">
+    <div className="w-1/2 h-1/2 bg-[#fff] rounded-lg m-2 p-2">
       <div>
-        <h3>Income</h3>
-        <h4>amount shown here</h4>
+        <h3>Total Debt</h3>
+        <h4>£ {debt}</h4>
       </div>
       <div>
-        <h3>Outgoings</h3>
-        <h4>£v amount shown here</h4>
+        <h3>Total Income</h3>
+        <h4>£ amount shown here</h4>
       </div>
       <div>
-        <h3>Outgoings</h3>
-        <h4>£v amount shown here</h4>
+        <h3>Total Monthly Outgoings</h3>
+        <h4>{item}</h4>
+      </div>
+      <div>
+        <h3>Months to pay off debt</h3>
+        <h4>total months and date show here</h4>
       </div>
     </div>
   );
