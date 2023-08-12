6d293a5cc5a19788f13455341378fc55ea0e8d3f
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-08-12 09:37:06 +0100

    swapped out all local dummy data for backend data, posting to and fetching from financials api successfully

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import Button from "./components/Button";
@@ -6,37 +6,52 @@ import {
   incomeInput,
   mainHeading,
   buttonEnter,
-  buttonDanger,
 } from "./tailwind_classes/classNames";
 import TotalsModal from "./components/TotalsModal";
 
 function App() {
   const [debt, setDebt] = useState("");
-  const [debtAmount, setDebtAmount] = useState({});
   const [income, setIncome] = useState("");
-  const [incomeAmount, setIncomeAmount] = useState({});
   const [outAmount, setOutAmount] = useState("");
-  const [outgoingsObject, setOutgoingsObject] = useState({});
+  const [incomeAmount, setIncomeAmount] = useState(0);
+  const [debtAmount, setDebtAmount] = useState(0);
+  const [outgoingsAmount, setOutgoingsAmount] = useState(0);
+  const [balance, setBalance] = useState(0);
+  const [months, setMonths] = useState(0);
 
-  function handleDebtAmount() {
-    const debtAsNumber = Number(debt);
-    setDebtAmount({ id: Math.random() * 100, number: debtAsNumber });
-    setDebt("");
+  const financialsUrl = "http://localhost:5000/api/v1/financials";
+
+  async function getFinancials() {
+    const response = await fetch(financialsUrl);
+    const data = await response.json();
+    console.log("fetched data:", data);
+    setDebtAmount(data.payload[data.payload.length - 1].debt);
+    setIncomeAmount(data.payload[data.payload.length - 1].income);
+    setOutgoingsAmount(data.payload[data.payload.length - 1].outgoings);
+    setBalance(data.payload[data.payload.length - 1].balance);
+    setMonths(data.payload[data.payload.length - 1].months);
   }
+  getFinancials();
 
-  function handleIncomeAmount() {
-    const incomeAsNumber = Number(income);
-    setIncomeAmount({ id: Math.random() * 100, number: incomeAsNumber });
-    setIncome("");
-  }
-
-  function handleOutgoings() {
-    const outgoingsAsNumber = Number(outAmount);
-    setOutgoingsObject({
-      id: Math.random() * 100,
-      amount: outgoingsAsNumber,
+  async function handleFinancials() {
+    let balance = income - outAmount;
+    const response = await fetch(financialsUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        debt: debt,
+        income: income,
+        outgoings: outAmount,
+        balance: balance,
+        months: Math.floor(debt / balance),
+      }),
     });
+    const data = await response.json();
+    console.log("posted data:", data);
+    setDebt("");
+    setIncome("");
     setOutAmount("");
+    getFinancials();
   }
 
   return (
@@ -52,53 +67,39 @@ function App() {
         placeholder="enter debt"
         changeEvent={(event) => setDebt(event.target.value)}
       />
-      <Button
-        clickEvent={() => {
-          handleDebtAmount();
-        }}
-        buttonStyling={buttonEnter}
-      />
 
-      {debtAmount.number > 0 && (
-        <div>
-          <h2 className="ml-2">What is your salary after tax?</h2>
-          <Input
-            value={income}
-            type="number"
-            inputStyling={incomeInput}
-            placeholder="enter salary"
-            changeEvent={(event) => setIncome(event.target.value)}
-          />
-          <Button
-            clickEvent={() => {
-              handleIncomeAmount();
-            }}
-            buttonStyling={buttonEnter}
-          />
-        </div>
-      )}
-      {incomeAmount.number > 0 && (
-        <div>
-          <h2 className="ml-2">
-            Total Monthly Outgoings (eg. phone, travel, rent)
-          </h2>
-          <Input
-            value={outAmount}
-            type="number"
-            inputStyling={incomeInput}
-            placeholder="enter amount"
-            changeEvent={(event) => setOutAmount(event.target.value)}
-          />
-          <Button clickEvent={handleOutgoings} buttonStyling={buttonDanger} />
-        </div>
-      )}
+      <div>
+        <h2 className="ml-2">What is your salary after tax?</h2>
+        <Input
+          value={income}
+          type="number"
+          inputStyling={incomeInput}
+          placeholder="enter salary"
+          changeEvent={(event) => setIncome(event.target.value)}
+        />
+      </div>
+
+      <div>
+        <h2 className="ml-2">
+          Total Monthly Outgoings (eg. phone, travel, rent)
+        </h2>
+        <Input
+          value={outAmount}
+          type="number"
+          inputStyling={incomeInput}
+          placeholder="enter amount"
+          changeEvent={(event) => setOutAmount(event.target.value)}
+        />
+        <Button clickEvent={handleFinancials} buttonStyling={buttonEnter} />
+      </div>
+
       <div>
         <TotalsModal
+          outgoingsAmount={outgoingsAmount}
           debtAmount={debtAmount}
-          debt={debt}
-          outgoingsObject={outgoingsObject}
-          income={income}
           incomeAmount={incomeAmount}
+          months={months}
+          balance={balance}
         />
       </div>
     </div>
--- src/components/TotalsModal.jsx
+++ src/components/TotalsModal.jsx
@@ -2,47 +2,37 @@ import React from "react";
 import { currency } from "../helper-functions/currency";
 import { getFutureDate } from "../helper-functions/futureDate";
 
-const TotalsModal = ({ debtAmount, incomeAmount, outgoingsObject }) => {
-  let salaryBalance = incomeAmount.number - outgoingsObject.amount;
-  let monthsToPayDebt = debtAmount.number / salaryBalance;
-
+const TotalsModal = ({
+  outgoingsAmount,
+  debtAmount,
+  incomeAmount,
+  months,
+  balance,
+}) => {
   return (
     <div>
-      {debtAmount?.number > 0 && (
+      {months > 0 && (
         <div className="w-1/2 h-1/2 bg-[#fff] rounded-lg m-2 p-2">
-          {debtAmount?.number > 0 && (
-            <div>
-              <h3 className="font-bold">Total Debt</h3>
-              <h4 className="mb-2">{currency(debtAmount?.number)}</h4>
-            </div>
-          )}
-          {incomeAmount.number > 0 && (
-            <div>
-              <h3 className="font-bold">Total Income</h3>
-              <h4 className="mb-2">{currency(incomeAmount?.number)}</h4>
-            </div>
-          )}
-          {outgoingsObject.amount > 0 && (
-            <div>
-              <h3 className="font-bold">Total Monthly Outgoings</h3>
-              <h4 className="mb-2">{currency(outgoingsObject?.amount)}</h4>
-            </div>
-          )}
-          {salaryBalance > 0 && (
-            <div>
-              <h3 className="font-bold">Salary balance after outgoings:</h3>
-              <h3 className="mb-2">{currency(salaryBalance)}</h3>
-            </div>
-          )}
-          {monthsToPayDebt > 0 && (
-            <div>
-              <h3 className="font-bold">
-                Debt free in {Math.floor(monthsToPayDebt)} Months :
-              </h3>
-
-              {getFutureDate(monthsToPayDebt)}
-            </div>
-          )}
+          <div>
+            <h3 className="font-bold">Total Debt</h3>
+            <h4 className="mb-2">{currency(debtAmount)}</h4>
+          </div>
+          <div>
+            <h3 className="font-bold">Total Income</h3>
+            <h4 className="mb-2">{currency(incomeAmount)}</h4>
+          </div>
+          <div>
+            <h3 className="font-bold">Total Monthly Outgoings</h3>
+            <h4 className="mb-2">{currency(outgoingsAmount)}</h4>
+          </div>
+          <div>
+            <h3 className="font-bold">Salary balance after outgoings:</h3>
+            <h3 className="mb-2">{currency(balance)}</h3>
+          </div>
+          <div>
+            <h3 className="font-bold">Debt free in {months} Months :</h3>
+            {getFutureDate(months)}
+          </div>
         </div>
       )}
     </div>
